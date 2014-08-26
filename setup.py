5eb76679af8d77d4cf70235e532c0ba5a2a23af3
Andy Grabow <andy@freilandkiwis.de>
2014-08-26 12:34:10 +0200

    make it actually usable :)

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 setup(
     name='django-undeletable',
-    packages=['django-undeletable'],  # this must be the same as the name above
-    version='0.1.1',
+    packages=['django_undeletable'],  # this must be the same as the name above
+    version='0.1.2',
     description0='Deleted data stays in the database and will be hidden from the default manager.',
     author='Andy Grabow',
     author_email='andy@freilandkiwis.de',
     url='https://github.com/kakulukia/django-undeletable',  # use the URL to the github repo
-    download_url='https://github.com/kakulukia/django-undeletable/tarball/0.1.1',  # I'll explain this in a second
+    download_url='https://github.com/kakulukia/django-undeletable/tarball/0.1.2',  # I'll explain this in a second
     keywords=['orm', 'undelete', 'shadow db'],  # arbitrary keywords
     classifiers=[],
 )
