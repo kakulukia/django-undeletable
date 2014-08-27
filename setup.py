87892ee0c72782332d669686a1695bcbcec686ea
Andy Grabow <andy@freilandkiwis.de>
2014-08-27 12:34:09 +0200

    new version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='django-undeletable',
     packages=['django_undeletable'],  # this must be the same as the name above
-    version='0.1.2',
+    version='0.1.3',
     description0='Deleted data stays in the database and will be hidden from the default manager.',
     author='Andy Grabow',
     author_email='andy@freilandkiwis.de',
     url='https://github.com/kakulukia/django-undeletable',  # use the URL to the github repo
-    download_url='https://github.com/kakulukia/django-undeletable/tarball/0.1.2',  # I'll explain this in a second
+    download_url='https://github.com/kakulukia/django-undeletable/tarball/0.1.3',  # I'll explain this in a second
     keywords=['orm', 'undelete', 'shadow db'],  # arbitrary keywords
     classifiers=[],
 )
