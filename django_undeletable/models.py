09eafdb4134c88b547d1d1baa6e219923d416b1f
Andy Grabow <andy@freilandkiwis.de>
2014-09-05 15:30:14 +0200

    adding a custom user model to work with the data manager

--- django_undeletable/models.py
+++ django_undeletable/models.py
@@ -1,14 +1,19 @@
 # coding=utf-8
-from django.db import models
 from django.db.models.query import QuerySet
-from django.utils.datetime_safe import datetime
+from django.conf import settings
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+from django.core.mail import send_mail
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from django.core import validators
 
 
 # basic model stuff
 ##########################################
 class DataQuerySet(QuerySet):
     def delete(self):
-        self.update(active=False, deleted=datetime.now())
+        self.update(active=False, deleted=timezone.now())
 
 
 class AllObjectsQuerySet(QuerySet):
@@ -24,7 +29,7 @@ class DataManager(models.Manager):
         return qs.filter(active=True)
 
     def delete(self):
-        self.update(active=False, deleted=datetime.now())
+        self.update(active=False, deleted=timezone.now())
 
 
 class AllObjectsManager(models.Manager):
@@ -68,7 +73,7 @@ class BaseModel(models.Model):
         if self.active:
             self.deleted = None
         elif not self.deleted:
-            self.deleted = datetime.now()
+            self.deleted = timezone.now()
 
         super(BaseModel, self).save(force_insert=force_insert, using=using,
                                     update_fields=update_fields)
@@ -87,3 +92,56 @@ class NamedModel(BaseModel):
 
     def __unicode__(self):
         return self.name
+
+
+class UserDataManager(UserManager, DataManager):
+    pass
+
+
+class User(AbstractBaseUser, BaseModel, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=30, unique=True,
+                                help_text=_('user.login.username_help'),
+                                validators=[
+                                    validators.RegexValidator(
+                                        r'^[\w.@+-]+$', _('forms.errors.enter_valid_username.'), 'username_invalid')
+                                ])
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), blank=False)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+                                    help_text=_('Designates whether this user should be treated as '
+                                                'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    data = UserDataManager()
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        """ Returns the first_name plus the last_name, with a space in between. """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """ Returns the short name for the user. """
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=settings.EMAIL_DEFAULT_FROM, **kwargs):
+        """
+         Sends an email to this User.
+         If settings.EMAIL_OVERRIDE_ADDRESS is set, this mail will be redirected to the alternate mail address.
+
+        """
+        receiver = self.email
+        if settings.EMAIL_OVERRIDE_ADDRESS:
+            receiver = settings.EMAIL_OVERRIDE_ADDRESS
+
+        send_mail(subject, message, from_email, [receiver], **kwargs)
