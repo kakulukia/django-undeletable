450674325f8e7b6baef6af7fa27ec85faaab0f76
Andy Grabow <andy@freilandkiwis.de>
2018-02-25 16:39:12 +0100

    marking the package as stable

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
